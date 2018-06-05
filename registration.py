902ccff651fb215ad4e3e8f28e77cacc9347b8b3
sameer0000 <39871661+sameer0000@users.noreply.github.com>
2018-06-05 16:15:29 +0530

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,24 @@
+<!Doctypehtml>
+<html>
+<head>
+<title >
+Registration form
+</title>
+<link
+  rel="stylesheet"
+  href="style.css">
+</head>
+<body>
+<style> p,h3 color: blue; </style> 
+<h1> <p> Registration form.</p> </h1>
+<th> <h3>  Name :   <input type="text" name="name"> </h3></th> 
+<th>  <h3> college : <input type="text" name="college"></h3> </th>
+<th> <h3> Branch : <input type="text" name="branch"> </h3></th>
+<th> <h3> year : <input type="text" name="year"></h3></th>
+<th> <h3> Tracks : </h3></th>
+<tr>  <ul> <th> <li> Software  </li>  </th> </ul> 
+   <ul> <th> <li> Hardware  </li> </th> </ul>  </tr>
+
+</body>
+</html>
+
